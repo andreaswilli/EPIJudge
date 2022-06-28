ea76f44a59968b5bfb34edbf368324ef94835bb0
Andreas Willi <andiwilli8460@gmail.com>
2022-06-29 00:57:51 +0200

    Add 4.4 - closest integer with same weight

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
 
+# iterate through bits until they differ and swap them
+# time complexity: O(n)
+"""
 def closest_int_same_bit_count(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    i = 0
+    while (x >> i) & 1 == (x >> i+1) & 1:
+        i += 1
+    x ^= 0b11 << i
+    return x
+"""
+
+# find least significant differing bits and swap them
+# time complexity: O(1)
+def closest_int_same_bit_count(x: int) -> int:
+    i = (x&-x).bit_length() - 1 # least significant on bit index
+    if i == 0:
+        i = (~x&-~x).bit_length() - 1 # least significant off bit index
+    x ^= 0b11 << i-1
+    return x
 
 
 if __name__ == '__main__':
