18c31588f3e78a3d6ff31a01c49910005f616706
Andreas Willi <andiwilli8460@gmail.com>
2022-06-29 20:17:50 +0200

    Add 4.6 - quotient without arithmetical operators

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
 
+# repeatedly subtract y from x and count
+# time complexity: very high, takes x/y iterations
+"""
 def divide(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = 0
+    while x >= y:
+        x -= y
+        result += 1
+    return result
+"""
+
+# repeatedly subtract the largest 2^(k)y smaller than x
+# time complexity: O(n)
+def divide(x: int, y: int) -> int:
+    result = 0
+    power = 32
+
+    while x >= y:
+        while (y << power) > x:
+            power -= 1
+        x -= y << power
+        result += 1 << power
+    return result
 
 
 if __name__ == '__main__':
