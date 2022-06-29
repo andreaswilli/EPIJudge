42710dbabf532fd15bf156385daf8bede0472a9a
Andreas Willi <andiwilli8460@gmail.com>
2022-06-29 19:10:19 +0200

    Add 4.5 - product without arithmetical operators

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -1,12 +1,32 @@
 from test_framework import generic_test
 
+def add(a: int, b: int) -> int:
+    return a if b == 0 else add(a^b, (a&b) << 1)
 
+# repeatedly add
+# time complexity: O(2^n)
+"""
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = 0
+    for _ in range(x):
+        result = add(y, result)
+    return result
+"""
+
+# add 2^(k)y for every set bit in x
+# time complexity: O(n^2)
+def multiply(x: int, y: int) -> int:
+    result = 0
+    while x:
+        if x & 1:
+            result = add(result, y)
+        x >>= 1
+        y <<= 1
+    return result
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('primitive_multiply.py',
                                        'primitive_multiply.tsv', multiply))
+
