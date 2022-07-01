833096a8b58003852e65dc8912ac9d23b75870cb
Andreas Willi <andiwilli8460@gmail.com>
2022-07-01 15:57:55 +0200

    Add 4.8 - reverse digits

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
 
+# string manipulation
+# time complexity: O(n)
+"""
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    sign = -1 if x < 0 else 1
+    return sign * int(str(abs(x))[::-1])
+"""
+
+# using mod 10
+# time complexity: O(n)
+def reverse(x: int) -> int:
+    result = 0
+    sign = -1 if x < 0 else 1
+    x = abs(x)
+    while x > 0:
+        result *= 10
+        result += x % 10
+        x //= 10
+    return sign * result
 
 
 if __name__ == '__main__':
