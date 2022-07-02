6fba69e2e1f184aab9cd5ff24e91f70711827803
Andreas Willi <andiwilli8460@gmail.com>
2022-07-02 15:41:14 +0200

    Add 4.11 - rectangle intersection

--- epi_judge_python/rectangle_intersection.py
+++ epi_judge_python/rectangle_intersection.py
@@ -5,10 +5,13 @@ from test_framework.test_failure import PropertyName
 
 Rect = collections.namedtuple('Rect', ('x', 'y', 'width', 'height'))
 
-
+# time O(1)
 def intersect_rectangle(r1: Rect, r2: Rect) -> Rect:
-    # TODO - you fill in here.
-    return Rect(0, 0, 0, 0)
+    width = min(r1.x + r1.width, r2.x + r2.width) - max(r1.x, r2.x)
+    height = min(r1.y + r1.height, r2.y + r2.height) - max(r1.y, r2.y)
+    if width >= 0 and height >= 0:
+        return Rect(max(r1.x, r2.x), max(r1.y, r2.y), width, height)
+    return Rect(0, 0, -1, -1)
 
 
 def intersect_rectangle_wrapper(r1, r2):
