805b9dfbb30ef7449b8168d79f97293e56f1ac1d
Andreas Willi <andiwilli8460@gmail.com>
2022-07-07 23:35:53 +0200

    Add 5.4 - advancing through array

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -3,8 +3,33 @@ from typing import List
 from test_framework import generic_test
 
 
+# brute force
+# time: O(n^2)
+# space: O(n)
+"""
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
+    reachable = [True] + [False] * (len(A) - 1)
+    for i in range(len(A)):
+        if not reachable[i]:
+            continue
+        if A[i] >= len(A) - i - 1:
+            return True
+        for j in range(A[i]):
+            reachable[i + j + 1] = True
+    return False
+"""
+
+# iterate once and keep track of furthest
+# time: O(n)
+# space: O(1)
+def can_reach_end(A: List[int]) -> bool:
+    furthest = 0
+    i = 0
+    while furthest < len(A) - 1:
+        furthest = max(furthest, i + A[i])
+        if furthest == i:
+            return False
+        i += 1
     return True
 
 
