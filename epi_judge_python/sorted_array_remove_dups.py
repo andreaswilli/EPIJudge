212b215560d50a52a37a5382c2f9820e69992be7
Andreas Willi <andiwilli8460@gmail.com>
2022-07-09 19:13:19 +0200

    Add 5.5 - delete duplicates from sorted array

--- epi_judge_python/sorted_array_remove_dups.py
+++ epi_judge_python/sorted_array_remove_dups.py
@@ -4,11 +4,50 @@ from typing import List
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
-
-# Returns the number of valid entries after deletion.
+# shift everything after a duplicte to the left by one
+# time: O(n^2)
+# space: O(1)
+"""
 def delete_duplicates(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_del = 0
+    i = 0
+    while i + num_del < len(A) - 1:
+        if A[i] == A[i + 1]:
+            num_del += 1
+            for j in range(i, len(A) - num_del):
+                A[j] = A[j + 1]
+        else:
+            i += 1
+    return len(A) - num_del
+"""
+
+# using a dict
+# time: O(n)
+# space: O(n)
+"""
+def delete_duplicates(A: List[int]) -> int:
+    res = []
+    occurred = {}
+    for i in range(len(A)):
+        if not A[i] in occurred:
+            res.append(A[i])
+            occurred[A[i]] = True
+    A[:] = res
+    return len(res)
+"""
+
+# reduce number of shifts
+# time: O(n)
+# space: O(1)
+def delete_duplicates(A: List[int]) -> int:
+    if (len(A) == 0):
+        return 0
+    write = 1
+    for i in range(len(A)):
+        if A[i] != A[write - 1]:
+            A[write] = A[i]
+            write += 1
+    return write
 
 
 @enable_executor_hook
