45085a3fd384b0168de0fdf023314653d99b7ab3
Andreas Willi <andiwilli8460@gmail.com>
2022-07-16 21:34:34 +0200

    Add 5.9 - enumerate primes

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -1,12 +1,38 @@
 from typing import List
+import math
 
 from test_framework import generic_test
 
 
-# Given n, return all primes up to and including n.
+# brute force
+# time: O(n * sqrt(n))
+# space: O(1)
+"""
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    primes = []
+    for i in range(2, n + 1):
+        isPrime = True
+        for j in range(2, math.floor(math.sqrt(i)) + 1):
+            if i % j == 0:
+                isPrime = False
+                break;
+        if isPrime:
+            primes.append(i)
+    return primes
+"""
+
+# remove multiples of primes
+# time: O(n log log n)
+# space: O(n)
+def generate_primes(n: int) -> List[int]:
+    is_prime = [False] * 2 + [True] * n
+    primes = []
+    for i in range(2, n + 1):
+        if is_prime[i]:
+            primes.append(i)
+            for j in range(2 * i, n + 1, i):
+                is_prime[j] = False
+    return primes
 
 
 if __name__ == '__main__':
