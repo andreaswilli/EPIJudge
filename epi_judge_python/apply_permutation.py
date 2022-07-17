43bd9ee5a6ae5cf8b3e2ea56f698c37c6dfd570c
Andreas Willi <andiwilli8460@gmail.com>
2022-07-18 01:12:53 +0200

    Add 5.10 - permute elements of array

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -3,8 +3,45 @@ from typing import List
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(n)
+"""
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
+    result = [0] * len(A)
+    for i in range(len(perm)):
+        result[perm[i]] = A[i]
+    A[:] = result
+    return
+"""
+
+# swap items in both lists one by one
+# time: O(n)
+# space: O(1), or O(n) if perm cannot be mutated
+"""
+def apply_permutation(perm: List[int], A: List[int]) -> None:
+    i = 0
+    while i < len(A):
+        if perm[i] == i:
+            i += 1
+        else:
+            A[perm[i]], A[i] = A[i], A[perm[i]]
+            perm[perm[i]], perm[i] = perm[i], perm[perm[i]]
+    return
+"""
+
+# swap items in A one by one (and restore perm list)
+# time: O(n)
+# space: O(1)
+def apply_permutation(perm, A) -> None:
+    for i in range(len(A)):
+        cur = i
+        while perm[cur] >= 0:
+            A[i], A[perm[cur]] = A[perm[cur]], A[i]
+            prev = cur
+            cur = perm[cur]
+            perm[prev] = -1 * perm[prev] - 1
+    for i in range(len(A)):
+        perm[i] = -1 * (perm[i] + 1)
     return
 
 
