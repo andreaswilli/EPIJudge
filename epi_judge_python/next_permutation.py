6bcc7758607dda8d4da53cee7c67852147ed6e6e
Andreas Willi <andiwilli8460@gmail.com>
2022-07-19 01:44:24 +0200

    Add 5.11 - compute next permutation

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -3,9 +3,20 @@ from typing import List
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(1)
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    desc_idx = len(perm) - 2
+    while desc_idx >= 0 and perm[desc_idx + 1] <= perm[desc_idx]:
+        desc_idx -= 1
+    if desc_idx == -1:
+        return []
+    for i in reversed(range(desc_idx + 1, len(perm))):
+        if perm[i] > perm[desc_idx]:
+            perm[desc_idx], perm[i] = perm[i], perm[desc_idx]
+            break
+    perm[desc_idx + 1:] = list(reversed(perm[desc_idx + 1:]))
+    return perm
 
 
 if __name__ == '__main__':
