02af538a3ca066f3f2bf2316e8d189607a34c105
Andreas Willi <andiwilli8460@gmail.com>
2022-07-19 19:22:58 +0200

    Add 5.12 - sample offline data

--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -1,4 +1,5 @@
 import functools
+import random
 from typing import List
 
 from test_framework import generic_test
@@ -8,8 +9,44 @@ from test_framework.random_sequence_checker import (
 from test_framework.test_utils import enable_executor_hook
 
 
+# repeatedly pick a random number from A (pick again if number is picked twice)
+# time: O(k)
+# space: O(k)
+"""
 def random_sampling(k: int, A: List[int]) -> None:
-    # TODO - you fill in here.
+    result = []
+    for _ in range(k):
+        pick = None
+        while pick == None or pick in result:
+            pick = random.choice(A)
+        result.append(pick)
+    A[:] = result
+    return
+"""
+
+# repeatedly pick a random number from A and swap it to the start of A
+# time: O(k)
+# space: O(1)
+"""
+def random_sampling(k: int, A: List[int]) -> None:
+    for i in range(k):
+        pick = random.randint(i, len(A) - 1)
+        A[i], A[pick] = A[pick], A[i]
+    return
+"""
+
+# optimize for k > n/2
+# time: O(min(k, n-k))
+# space: O(1)
+def random_sampling(k: int, A: List[int]) -> None:
+    if k <= len(A) / 2:
+        for i in range(k):
+            pick = random.randint(i, len(A) - 1)
+            A[i], A[pick] = A[pick], A[i]
+    else:
+        for i in range(len(A) - k):
+            pick = random.randint(0, len(A) - 1 - i)
+            A[len(A) - 1 -i], A[pick] = A[pick], A[len(A) - 1 -i]
     return
 
 
