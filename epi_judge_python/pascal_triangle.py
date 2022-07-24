4a61b901053693d9520ac381e6ca037017e9a103
Andreas Willi <andiwilli8460@gmail.com>
2022-07-25 00:28:15 +0200

    Add 5.20 - pascals triangle

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -3,13 +3,19 @@ from typing import List
 from test_framework import generic_test
 
 
+# time: O(n^2)
+# space: O(1)
 def generate_pascal_triangle(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    triangle = [[1] * x for x in range(1, n + 1)]
+    for i in range(n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    return triangle
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('pascal_triangle.py',
-                                       'pascal_triangle.tsv',
-                                       generate_pascal_triangle))
+        generic_test.generic_test_main(
+            "pascal_triangle.py", "pascal_triangle.tsv", generate_pascal_triangle
+        )
+    )
