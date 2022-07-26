7045b5b79a45d435db50236d7508db6939eb9815
Andreas Willi <andiwilli8460@gmail.com>
2022-07-26 22:34:46 +0200

    Add 6.2 - base conversion

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,12 +1,27 @@
 from test_framework import generic_test
 
 
+# time: O(n(1 + log_b2(b1)))
 def convert_base(num_as_string: str, b1: int, b2: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    if num_as_string == "0":
+        return "0"
+    numerals = "0123456789ABCDEF"
+    isNegative = num_as_string[0] == "-"
+    num = 0
+    for n in num_as_string[isNegative:]:
+        num = num * b1 + numerals.index(n)
+    result = []
+    while num > 0:
+        result.append(numerals[num % b2])
+        num //= b2
+    if isNegative:
+        result.append("-")
+    return "".join(reversed(result))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('convert_base.py', 'convert_base.tsv',
-                                       convert_base))
+        generic_test.generic_test_main(
+            "convert_base.py", "convert_base.tsv", convert_base
+        )
+    )
