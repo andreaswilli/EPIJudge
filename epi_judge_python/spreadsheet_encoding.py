02d7f38a395a74e68c016721750be0476c7e9981
Andreas Willi <andiwilli8460@gmail.com>
2022-07-27 23:41:05 +0200

    Add 6.3 - spreadsheet column encoding

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -1,13 +1,27 @@
 from test_framework import generic_test
+import functools
 
 
+# for loop
+# time: O(n)
+# space: O(1)
+# def ss_decode_col_id(col: str) -> int:
+#     result = 0
+#     for c in col:
+#         result = result * 26 + ord(c) - ord("A") + 1
+#     return result
+
+
+# reduce function
+# time: O(n)
+# space: O(1)
 def ss_decode_col_id(col: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    return functools.reduce(lambda acc, cur: acc * 26 + ord(cur) - ord("A") + 1, col, 0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('spreadsheet_encoding.py',
-                                       'spreadsheet_encoding.tsv',
-                                       ss_decode_col_id))
+        generic_test.generic_test_main(
+            "spreadsheet_encoding.py", "spreadsheet_encoding.tsv", ss_decode_col_id
+        )
+    )
