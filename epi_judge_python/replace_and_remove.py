02f79b4e8d0b50658769661f8638a54a10d8104b
Andreas Willi <andiwilli8460@gmail.com>
2022-07-29 23:30:49 +0200

    Add 6.4 - replace and remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,9 +5,61 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+# time: O(n^2)
+# space: O(1)
+# def replace_and_remove(size: int, s: List[str]) -> int:
+#     i = 0
+#     while i < len(s):
+#         if s[i] == "b":
+#             del s[i]
+#             size -= 1
+#         elif s[i] == "a":
+#             s[i] = "d"
+#             s.insert(i, "d")
+#             size += 1
+#             i += 2
+#         else:
+#             i += 1
+#     return size
+
+
+# time: O(n)
+# space: O(n)
+# def replace_and_remove(size: int, s: List[str]) -> int:
+#     result = []
+#     for i in range(size):
+#         if s[i] == "b":
+#             continue
+#         if s[i] == "a":
+#             result.append("d")
+#             result.append("d")
+#         else:
+#             result.append(s[i])
+#     s[:] = result
+#     return len(result)
+
+
+# time: O(n)
+# space: O(1)
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    write_idx = 0
+    a_count = 0
+    for i in range(size):
+        if s[i] != "b":
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == "a":
+            a_count += 1
+    write_idx += a_count - 1
+    size = write_idx + 1
+    for i in reversed(range(size - a_count)):
+        if s[i] == "a":
+            s[write_idx - 1 : write_idx + 1] = "dd"
+            write_idx -= 2
+        else:
+            s[write_idx] = s[i]
+            write_idx -= 1
+    return size
 
 
 @enable_executor_hook
@@ -16,8 +68,11 @@ def replace_and_remove_wrapper(executor, size, s):
     return s[:res_size]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('replace_and_remove.py',
-                                       'replace_and_remove.tsv',
-                                       replace_and_remove_wrapper))
+        generic_test.generic_test_main(
+            "replace_and_remove.py",
+            "replace_and_remove.tsv",
+            replace_and_remove_wrapper,
+        )
+    )
