edeaae2c6fc60eec0205b7bb726b07893c937167
Andreas Willi <andiwilli8460@gmail.com>
2022-08-04 20:34:06 +0200

    Add 6.8 - roman to decimal

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -1,13 +1,25 @@
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(1)
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    sum = 0
+    i = 0
+    while i < len(s):
+        if i + 1 == len(s) or numerals[s[i]] >= numerals[s[i + 1]]:
+            sum += numerals[s[i]]
+            i += 1
+        else:
+            sum += numerals[s[i + 1]] - numerals[s[i]]
+            i += 2
+    return sum
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('roman_to_integer.py',
-                                       'roman_to_integer.tsv',
-                                       roman_to_integer))
+        generic_test.generic_test_main(
+            "roman_to_integer.py", "roman_to_integer.tsv", roman_to_integer
+        )
+    )
