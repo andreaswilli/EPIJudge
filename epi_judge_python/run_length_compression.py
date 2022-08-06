4ff32cf3370d9dee4fced65f6cdc6c7527bcca72
Andreas Willi <andiwilli8460@gmail.com>
2022-08-06 09:39:44 +0200

    Add 6.11 - run-length encoding

--- epi_judge_python/run_length_compression.py
+++ epi_judge_python/run_length_compression.py
@@ -2,25 +2,43 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
+# time: O(n)
+# space: O(n)
 def decoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    result = []
+    start = i = 0
+    while i < len(s):
+        while s[i].isdigit():
+            i += 1
+        result.append(s[i] * int(s[start:i]))
+        i += 1
+        start = i
+    return "".join(result)
 
 
+# time: O(n)
+# space: O(n)
 def encoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    result = []
+    start = cur = 0
+    while cur < len(s):
+        while cur < len(s) and s[start] == s[cur]:
+            cur += 1
+        result.append(str(cur - start) + s[cur - 1])
+        start = cur
+    return "".join(result)
 
 
 def rle_tester(encoded, decoded):
     if decoding(encoded) != decoded:
-        raise TestFailure('Decoding failed')
+        raise TestFailure("Decoding failed")
     if encoding(decoded) != encoded:
-        raise TestFailure('Encoding failed')
+        raise TestFailure("Encoding failed")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('run_length_compression.py',
-                                       'run_length_compression.tsv',
-                                       rle_tester))
+        generic_test.generic_test_main(
+            "run_length_compression.py", "run_length_compression.tsv", rle_tester
+        )
+    )
