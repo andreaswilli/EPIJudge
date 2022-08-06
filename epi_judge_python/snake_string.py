0a705b606c254685d9caa57dd437cc6dc41205a1
Andreas Willi <andiwilli8460@gmail.com>
2022-08-06 09:09:44 +0200

    Add 6.10 - snake string

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -1,12 +1,29 @@
 from test_framework import generic_test
+import math
 
 
+# time: O(n)
+# space: O(n)
+# def snake_string(s: str) -> str:
+#     snake = []
+#     for i in range(1, len(s), 4):
+#         snake.append(s[i])
+#     for i in range(0, len(s), 2):
+#         snake.append(s[i])
+#     for i in range(3, len(s), 4):
+#         snake.append(s[i])
+#     return "".join(snake)
+
+
+# time: O(n)
+# space: O(n)
 def snake_string(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    return s[1::4] + s[::2] + s[3::4]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('snake_string.py', 'snake_string.tsv',
-                                       snake_string))
+        generic_test.generic_test_main(
+            "snake_string.py", "snake_string.tsv", snake_string
+        )
+    )
