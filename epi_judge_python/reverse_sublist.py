650836c7245419becdfb812e37fb504731b1396e
Andreas Willi <andiwilli8460@gmail.com>
2022-08-10 22:46:11 +0200

    Add 7.2 - reverse a single sublist

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -3,14 +3,23 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
+# time: O(n)
+# space: O(1)
+def reverse_sublist(L: ListNode, start: int, finish: int) -> Optional[ListNode]:
+    dummy_head = before_sub = ListNode(0, L)
+    for _ in range(1, start):
+        before_sub = before_sub.next
 
-def reverse_sublist(L: ListNode, start: int,
-                    finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    new_sub_tail = before_sub.next
+    for _ in range(start, finish):
+        tmp = new_sub_tail.next
+        new_sub_tail.next, tmp.next, before_sub.next = tmp.next, before_sub.next, tmp
+    return dummy_head.next
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('reverse_sublist.py',
-                                       'reverse_sublist.tsv', reverse_sublist))
+        generic_test.generic_test_main(
+            "reverse_sublist.py", "reverse_sublist.tsv", reverse_sublist
+        )
+    )
