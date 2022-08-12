7df739579c06ce8884933f21c15c18c85cc70488
Andreas Willi <andiwilli8460@gmail.com>
2022-08-12 22:16:51 +0200

    Add 7.3 - test for cyclicity

--- epi_judge_python/is_list_cyclic.py
+++ epi_judge_python/is_list_cyclic.py
@@ -7,8 +7,82 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+# time: O(n^2)
+# space: O(n)
+# def has_cycle(head: ListNode) -> Optional[ListNode]:
+#     visited = list()
+#     cur = head
+#     while cur:
+#         if cur in visited:
+#             return cur
+#         visited.append(cur)
+#         cur = cur.next
+#     return None
+
+# time: O(n^2)
+# space: O(1)
+# def has_cycle(head: ListNode) -> Optional[ListNode]:
+#     cur = head
+#     i = 0
+#     while cur:
+#         cur_inner = head
+#         j = 0
+#         while j < i and cur_inner:
+#             if cur.data == cur_inner.data:
+#                 return cur
+#             cur_inner = cur_inner.next
+#             j += 1
+#         cur = cur.next
+#         i += 1
+#     return None
+
+# time: O(n)
+# space: O(n)
+# def has_cycle(head: ListNode) -> Optional[ListNode]:
+#     visited = set()
+#     cur = head
+#     while cur:
+#         if cur.data in visited:
+#             return cur
+#         visited.add(cur.data)
+#         cur = cur.next
+#     return None
+
+# time: O(n)
+# space: O(1)
+# def has_cycle(head: ListNode) -> Optional[ListNode]:
+#     slow = fast = head
+#     while slow and fast and fast.next:
+#         slow = slow.next
+#         fast = fast.next.next
+#         if slow is fast:
+#             cycle_len = 0
+#             start = slow
+#             while slow:
+#                 cycle_len += 1
+#                 slow = slow.next
+#                 if slow is start:
+#                     break
+#             advanced_it = it = head
+#             for _ in range(cycle_len):
+#                 advanced_it = advanced_it.next if advanced_it else None
+#             while it and advanced_it and it is not advanced_it:
+#                 it = it.next
+#                 advanced_it = advanced_it.next
+#             return it
+#     return None
+
+# time: O(n)
+# space: O(1)
 def has_cycle(head: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
+    fast = slow = head
+    while slow and fast and fast.next and fast.next.next:
+        slow, fast = slow.next, fast.next.next
+        if slow is fast:
+            slow = head
+            while slow and slow is not fast:
+                slow, fast = slow.next, fast.next
+            return slow
     return None
 
 
@@ -17,7 +91,7 @@ def has_cycle_wrapper(executor, head, cycle_idx):
     cycle_length = 0
     if cycle_idx != -1:
         if head is None:
-            raise RuntimeError('Can\'t cycle empty list')
+            raise RuntimeError("Can't cycle empty list")
         cycle_start = None
         cursor = head
         while cursor.next is not None:
@@ -29,7 +103,7 @@ def has_cycle_wrapper(executor, head, cycle_idx):
         if cursor.data == cycle_idx:
             cycle_start = cursor
         if cycle_start is None:
-            raise RuntimeError('Can\'t find a cycle start')
+            raise RuntimeError("Can't find a cycle start")
         cursor.next = cycle_start
         cycle_length += 1
 
@@ -37,29 +111,30 @@ def has_cycle_wrapper(executor, head, cycle_idx):
 
     if cycle_idx == -1:
         if result is not None:
-            raise TestFailure('Found a non-existing cycle')
+            raise TestFailure("Found a non-existing cycle")
     else:
         if result is None:
-            raise TestFailure('Existing cycle was not found')
+            raise TestFailure("Existing cycle was not found")
         cursor = result
         while True:
             cursor = cursor.next
             cycle_length -= 1
             if cursor is None or cycle_length < 0:
                 raise TestFailure(
-                    'Returned node does not belong to the cycle or is not the closest node to the head'
+                    "Returned node does not belong to the cycle or is not the closest node to the head"
                 )
             if cursor is result:
                 break
 
     if cycle_length != 0:
         raise TestFailure(
-            'Returned node does not belong to the cycle or is not the closest node to the head'
+            "Returned node does not belong to the cycle or is not the closest node to the head"
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('is_list_cyclic.py',
-                                       'is_list_cyclic.tsv',
-                                       has_cycle_wrapper))
+        generic_test.generic_test_main(
+            "is_list_cyclic.py", "is_list_cyclic.tsv", has_cycle_wrapper
+        )
+    )
