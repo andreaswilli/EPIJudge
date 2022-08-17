22c3437f14ed90bfc47a79e01bd0e28c2337db4b
Andreas Willi <andiwilli8460@gmail.com>
2022-08-17 18:02:19 +0200

    Add 7.7 - remove kth last node from list

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -4,14 +4,27 @@ from list_node import ListNode
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(1)
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L: ListNode, k: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head = ListNode(0, L)
+    cur = L
+    for _ in range(k):
+        cur = cur.next
+    node_to_delete = dummy_head
+    while cur:
+        cur = cur.next
+        node_to_delete = node_to_delete.next
+    node_to_delete.next = node_to_delete.next.next
+    return dummy_head.next
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('delete_kth_last_from_list.py',
-                                       'delete_kth_last_from_list.tsv',
-                                       remove_kth_last))
+        generic_test.generic_test_main(
+            "delete_kth_last_from_list.py",
+            "delete_kth_last_from_list.tsv",
+            remove_kth_last,
+        )
+    )
