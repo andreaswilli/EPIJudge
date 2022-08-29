b5fd8f4caf787e8a1603067036c33e8770427c33
Andreas Willi <andiwilli8460@gmail.com>
2022-08-29 17:29:37 +0200

    Add 7.11 - is singly linked list palindromic

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -2,13 +2,29 @@ from list_node import ListNode
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(1)
 def is_linked_list_a_palindrome(L: ListNode) -> bool:
-    # TODO - you fill in here.
+    fast = slow = L
+    while slow and fast and fast.next:
+        slow, fast = slow.next, fast.next.next
+    dummy_head_rev = ListNode(0)
+    while slow:
+        dummy_head_rev.next, slow.next, slow = slow, dummy_head_rev.next, slow.next
+    first = L
+    second = dummy_head_rev.next
+    while first and second:
+        if first.data != second.data:
+            return False
+        first, second = first.next, second.next
     return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('is_list_palindromic.py',
-                                       'is_list_palindromic.tsv',
-                                       is_linked_list_a_palindrome))
+        generic_test.generic_test_main(
+            "is_list_palindromic.py",
+            "is_list_palindromic.tsv",
+            is_linked_list_a_palindrome,
+        )
+    )
