19cf9ecbde168837b273848e91f673e26da979c1
Andreas Willi <andiwilli8460@gmail.com>
2022-09-01 23:18:22 +0200

    Add 7.12 - list pivoting

--- epi_judge_python/pivot_list.py
+++ epi_judge_python/pivot_list.py
@@ -7,9 +7,27 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+# time: O(n)
+# space: O(1)
 def list_pivoting(l: ListNode, x: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head_lower = lower = ListNode()
+    dummy_head_pivot = pivot = ListNode()
+    dummy_head_greater = greater = ListNode()
+    while l:
+        if l.data < x:
+            lower.next = l
+            lower = lower.next
+        elif l.data == x:
+            pivot.next = l
+            pivot = pivot.next
+        else:
+            greater.next = l
+            greater = greater.next
+        l = l.next
+    greater.next = None
+    pivot.next = dummy_head_greater.next
+    lower.next = dummy_head_pivot.next
+    return dummy_head_lower.next
 
 
 def linked_to_list(l):
@@ -36,18 +54,20 @@ def list_pivoting_wrapper(executor, l, x):
                 mode = 1
         elif mode == 0:
             if i < x:
-                raise TestFailure('List is not pivoted')
+                raise TestFailure("List is not pivoted")
             elif i > x:
                 mode = 1
         else:
             if i <= x:
-                raise TestFailure('List is not pivoted')
+                raise TestFailure("List is not pivoted")
 
     if sorted(original) != sorted(pivoted):
-        raise TestFailure('Result list contains different values')
+        raise TestFailure("Result list contains different values")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('pivot_list.py', 'pivot_list.tsv',
-                                       list_pivoting_wrapper))
+        generic_test.generic_test_main(
+            "pivot_list.py", "pivot_list.tsv", list_pivoting_wrapper
+        )
+    )
