47dc4314bf98415fc82a4d15c6f2570df8715c82
Andreas Willi <andiwilli8460@gmail.com>
2022-09-03 13:07:25 +0200

    Add 8.3 - is a string well-formed

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,13 +1,28 @@
 from test_framework import generic_test
 
 
+# time: O(n)
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    parens = {
+        ")": "(",
+        "]": "[",
+        "}": "{",
+    }
+    stack = []
+    for p in s:
+        if p in parens:
+            if not stack or stack.pop() != parens[p]:
+                return False
+        else:
+            stack.append(p)
+    return not stack
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('is_valid_parenthesization.py',
-                                       'is_valid_parenthesization.tsv',
-                                       is_well_formed))
+        generic_test.generic_test_main(
+            "is_valid_parenthesization.py",
+            "is_valid_parenthesization.tsv",
+            is_well_formed,
+        )
+    )
