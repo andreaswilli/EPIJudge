20f8645ac4f06e6497f4000366fa282f907d763e
Andreas Willi <andiwilli8460@gmail.com>
2022-09-03 13:28:57 +0200

    Add 8.4 - normalize pathnames

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,13 +1,26 @@
 from test_framework import generic_test
 
 
+# time: O(n)
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    pack_names = []
+    for i, token in enumerate(path.split("/")):
+        if (i == 0 and token == "") or token not in ["", "."]:
+            if token == ".." and pack_names:
+                if pack_names[-1] == "..":
+                    pack_names.append(token)
+                else:
+                    pack_names.pop()
+            else:
+                pack_names.append(token)
+    return "/".join(pack_names) or "/"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('directory_path_normalization.py',
-                                       'directory_path_normalization.tsv',
-                                       shortest_equivalent_path))
+        generic_test.generic_test_main(
+            "directory_path_normalization.py",
+            "directory_path_normalization.tsv",
+            shortest_equivalent_path,
+        )
+    )
