6179c734d58dd673687624e4a4e88524abf2b3ef
Andreas Willi <andiwilli8460@gmail.com>
2022-09-03 12:52:36 +0200

    Add 8.2 - evaluate RPN expressions

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,12 +1,25 @@
 from test_framework import generic_test
 
 
+# time: O(n)
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    stack = []
+    operators = {
+        "+": lambda b, a: a + b,
+        "-": lambda b, a: a - b,
+        "*": lambda b, a: a * b,
+        "/": lambda b, a: a // b,
+    }
+
+    for token in expression.split(","):
+        if token in operators:
+            stack.append(operators[token](stack.pop(), stack.pop()))
+        else:
+            stack.append(int(token))
+    return stack.pop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('evaluate_rpn.py', 'evaluate_rpn.tsv',
-                                       evaluate))
+        generic_test.generic_test_main("evaluate_rpn.py", "evaluate_rpn.tsv", evaluate)
+    )
