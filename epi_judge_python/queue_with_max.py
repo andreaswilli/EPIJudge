b0a382e63c7b49b26218559155292728868013ba
Andreas Willi <andiwilli8460@gmail.com>
2022-09-15 19:19:45 +0200

    Add 8.9 - queue with max api

--- epi_judge_python/queue_with_max.py
+++ epi_judge_python/queue_with_max.py
@@ -1,19 +1,29 @@
+import collections
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
 class QueueWithMax:
+    entries = collections.deque()
+    potential_max = collections.deque()
+
+    # time: O(1) (amortized)
     def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        self.entries.append(x)
+        while self.potential_max and self.potential_max[-1] < x:
+            self.potential_max.pop()
+        self.potential_max.append(x)
 
+    # time: O(1)
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        value = self.entries.popleft()
+        if value == self.potential_max[0]:
+            self.potential_max.popleft()
+        return value
 
+    # time: O(1)
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        return self.potential_max[0]
 
 
 def queue_tester(ops):
@@ -22,27 +32,31 @@ def queue_tester(ops):
         q = QueueWithMax()
 
         for (op, arg) in ops:
-            if op == 'QueueWithMax':
+            if op == "QueueWithMax":
                 q = QueueWithMax()
-            elif op == 'enqueue':
+            elif op == "enqueue":
                 q.enqueue(arg)
-            elif op == 'dequeue':
+            elif op == "dequeue":
                 result = q.dequeue()
                 if result != arg:
-                    raise TestFailure('Dequeue: expected ' + str(arg) +
-                                      ', got ' + str(result))
-            elif op == 'max':
+                    raise TestFailure(
+                        "Dequeue: expected " + str(arg) + ", got " + str(result)
+                    )
+            elif op == "max":
                 result = q.max()
                 if result != arg:
-                    raise TestFailure('Max: expected ' + str(arg) + ', got ' +
-                                      str(result))
+                    raise TestFailure(
+                        "Max: expected " + str(arg) + ", got " + str(result)
+                    )
             else:
-                raise RuntimeError('Unsupported queue operation: ' + op)
+                raise RuntimeError("Unsupported queue operation: " + op)
     except IndexError:
-        raise TestFailure('Unexpected IndexError exception')
+        raise TestFailure("Unexpected IndexError exception")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('queue_with_max.py',
-                                       'queue_with_max.tsv', queue_tester))
+        generic_test.generic_test_main(
+            "queue_with_max.py", "queue_with_max.tsv", queue_tester
+        )
+    )
