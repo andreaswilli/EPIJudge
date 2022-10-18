5f7c55544450f60024813c823637a51759cc01e1
Andreas Willi <andiwilli8460@gmail.com>
2022-10-19 01:46:00 +0200

    Add 9.4 - LCA when nodes have parent pointers

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -1,3 +1,4 @@
+from collections import deque
 import functools
 from typing import Optional
 
@@ -8,25 +9,78 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def lca(node0: BinaryTreeNode,
-        node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+# time: O(h^2)
+# space: O(1)
+# def lca(node0: BinaryTreeNode, node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+#     it0 = node0
+#     it1: BinaryTreeNode
+#     while it0.parent:
+#         it1 = node1
+#         while it1.parent:
+#             if it0.data == it1.data:
+#                 return it0
+#             it1 = it1.parent
+#         it0 = it0.parent
+#     return it0
+
+# time: O(h)
+# space: O(h)
+# def lca(node0: BinaryTreeNode, node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+#     path0 = deque()
+#     path1 = deque()
+#     while node0:
+#         path0.appendleft(node0)
+#         node0 = node0.parent
+#     while node1:
+#         path1.appendleft(node1)
+#         node1 = node1.parent
+#     commonAncestor = None
+#     i = 0
+#     minPath = min(len(path0), len(path1))
+#     while i < minPath and path0[i] == path1[i]:
+#         commonAncestor = path0[i]
+#         i += 1
+#     return commonAncestor
+
+# time: O(h)
+# space: O(1)
+def lca(node0: BinaryTreeNode, node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+    def get_depth(node) -> int:
+        depth = 0
+        while node.parent:
+            depth += 1
+            node = node.parent
+        return depth
+
+    d0, d1 = get_depth(node0), get_depth(node1)
+
+    for _ in range(d0 - d1):
+        node0 = node0.parent
+    for _ in range(d1 - d0):
+        node1 = node1.parent
+
+    while node0 != node1:
+        node0, node1 = node0.parent, node1.parent
+
+    return node0
 
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, node0, node1):
     result = executor.run(
-        functools.partial(lca, must_find_node(tree, node0),
-                          must_find_node(tree, node1)))
+        functools.partial(lca, must_find_node(tree, node0), must_find_node(tree, node1))
+    )
 
     if result is None:
-        raise TestFailure('Result can\'t be None')
+        raise TestFailure("Result can't be None")
     return result.data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('lowest_common_ancestor_with_parent.py',
-                                       'lowest_common_ancestor.tsv',
-                                       lca_wrapper))
+        generic_test.generic_test_main(
+            "lowest_common_ancestor_with_parent.py",
+            "lowest_common_ancestor.tsv",
+            lca_wrapper,
+        )
+    )
