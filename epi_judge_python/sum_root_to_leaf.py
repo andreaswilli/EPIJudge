e07e2c746c695e71a5c08301ecb8d7dd7fee98dc
Andreas Willi <andiwilli8460@gmail.com>
2022-10-21 01:18:14 +0200

    Add 9.5 - sum root-to-leaf paths in binary tree

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -2,13 +2,24 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(h)
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
+    def traverse(tree: BinaryTreeNode, number: int) -> int:
+        if not tree:
+            return 0
+
+        new_number = number * 2 + tree.data
+        if tree.left or tree.right:
+            return traverse(tree.left, new_number) + traverse(tree.right, new_number)
+        return new_number
+
+    return traverse(tree, 0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('sum_root_to_leaf.py',
-                                       'sum_root_to_leaf.tsv',
-                                       sum_root_to_leaf))
+        generic_test.generic_test_main(
+            "sum_root_to_leaf.py", "sum_root_to_leaf.tsv", sum_root_to_leaf
+        )
+    )
