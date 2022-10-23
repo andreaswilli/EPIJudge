c6eecdb2b966cc6ccdcdf76fb6eeef4a4b306463
Andreas Willi <andiwilli8460@gmail.com>
2022-10-23 03:44:08 +0200

    Add 9.6 - find root to leaf path with specified sum

--- epi_judge_python/path_sum.py
+++ epi_judge_python/path_sum.py
@@ -2,12 +2,18 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-def has_path_sum(tree: BinaryTreeNode, remaining_weight: int) -> bool:
-    # TODO - you fill in here.
-    return True
+# time: O(n)
+# space: O(h)
+def has_path_sum(tree: BinaryTreeNode | None, remaining_weight: int) -> bool:
+    if not tree:
+        return False
+
+    if not tree.left and not tree.right:
+        return remaining_weight == tree.data
+
+    new_rem = remaining_weight - tree.data
+    return has_path_sum(tree.left, new_rem) or has_path_sum(tree.right, new_rem)
 
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('path_sum.py', 'path_sum.tsv',
-                                       has_path_sum))
+if __name__ == "__main__":
+    exit(generic_test.generic_test_main("path_sum.py", "path_sum.tsv", has_path_sum))
