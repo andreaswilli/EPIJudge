7e54874445285fcd10932158a0313f5975d6137e
Andreas Willi <andiwilli8460@gmail.com>
2022-11-06 18:27:43 +0100

    Add 9.9 - compute successor in tree

--- epi_judge_python/successor_in_tree.py
+++ epi_judge_python/successor_in_tree.py
@@ -7,9 +7,17 @@ from test_framework.binary_tree_utils import must_find_node
 from test_framework.test_utils import enable_executor_hook
 
 
+# time: O(h)
+# space: O(1)
 def find_successor(node: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    if node.right:
+        node = node.right
+        while node.left:
+            node = node.left
+        return node
+    while node.parent and node == node.parent.right:
+        node = node.parent
+    return node.parent
 
 
 @enable_executor_hook
@@ -21,8 +29,9 @@ def find_successor_wrapper(executor, tree, node_idx):
     return result.data if result else -1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('successor_in_tree.py',
-                                       'successor_in_tree.tsv',
-                                       find_successor_wrapper))
+        generic_test.generic_test_main(
+            "successor_in_tree.py", "successor_in_tree.tsv", find_successor_wrapper
+        )
+    )
