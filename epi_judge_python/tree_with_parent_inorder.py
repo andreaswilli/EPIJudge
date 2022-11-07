e16fd37faf642f3f4422e63bcc6e341b8e39b2bc
Andreas Willi <andiwilli8460@gmail.com>
2022-11-07 23:30:13 +0100

    Add 9.10 - inorder traversal with constant space

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -4,13 +4,50 @@ from binary_tree_with_parent_prototype import BinaryTreeNode
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(1)
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    while tree and tree.left:
+        tree = tree.left
+    while tree:
+        result.append(tree.data)
+        if tree.right:
+            tree = tree.right
+            while tree.left:
+                tree = tree.left
+        else:
+            while tree.parent and tree == tree.parent.right:
+                tree = tree.parent
+            tree = tree.parent
+    return result
 
 
-if __name__ == '__main__':
+# time: O(n)
+# space: O(1)
+# def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
+#     prev, result = None, []
+#     while tree:
+#         if prev is tree.parent:
+#             if tree.left:
+#                 next = tree.left
+#             else:
+#                 result.append(tree.data)
+#                 next = tree.right or tree.parent
+#         elif prev is tree.left:
+#             result.append(tree.data)
+#             next = tree.right or tree.parent
+#         else:
+#             next = tree.parent
+#         prev, tree = tree, next
+#     return result
+
+
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('tree_with_parent_inorder.py',
-                                       'tree_with_parent_inorder.tsv',
-                                       inorder_traversal))
+        generic_test.generic_test_main(
+            "tree_with_parent_inorder.py",
+            "tree_with_parent_inorder.tsv",
+            inorder_traversal,
+        )
+    )
