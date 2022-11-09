f351b3bd4f532754b306ae169db9a6f6769ccb4c
Andreas Willi <andiwilli8460@gmail.com>
2022-11-09 21:31:54 +0100

    Add 9.12 - reconstruct tree from traversal with null

--- epi_judge_python/tree_from_preorder_with_null.py
+++ epi_judge_python/tree_from_preorder_with_null.py
@@ -6,19 +6,33 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+# time: O(n)
+# space: O(1)
 def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    def reconstruct_preorder_helper(preorder_iter):
+        data = next(preorder_iter)
+        if data == None:
+            return None
+        return BinaryTreeNode(
+            data,
+            reconstruct_preorder_helper(preorder_iter),
+            reconstruct_preorder_helper(preorder_iter),
+        )
+
+    return reconstruct_preorder_helper(iter(preorder))
 
 
 @enable_executor_hook
 def reconstruct_preorder_wrapper(executor, data):
-    data = [None if x == 'null' else int(x) for x in data]
+    data = [None if x == "null" else int(x) for x in data]
     return executor.run(functools.partial(reconstruct_preorder, data))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('tree_from_preorder_with_null.py',
-                                       'tree_from_preorder_with_null.tsv',
-                                       reconstruct_preorder_wrapper))
+        generic_test.generic_test_main(
+            "tree_from_preorder_with_null.py",
+            "tree_from_preorder_with_null.tsv",
+            reconstruct_preorder_wrapper,
+        )
+    )
