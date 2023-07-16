14f14221866e780d3045d52405b3f16ee9902520
Andreas Willi <andiwilli8460@gmail.com>
2023-07-16 19:10:04 +0200

    Add 10.2 - sort increasing-decreasing array

--- epi_judge_python/sort_increasing_decreasing_array.py
+++ epi_judge_python/sort_increasing_decreasing_array.py
@@ -1,12 +1,35 @@
 from typing import List
+import heapq
 
 from test_framework import generic_test
 
 
+# time: O(n log k), where k is the number of lists
+# space: O(n)
 def sort_k_increasing_decreasing_array(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    was_increasing = True
+    sorted_lists = [[A[0]]]
 
+    for i in range(1, len(A)):
+        is_increasing = A[i-1] <= A[i]
+        if was_increasing == is_increasing:
+            sorted_lists[-1].append(A[i])
+        else:
+            if not was_increasing:
+                sorted_lists[-1] = list(reversed(sorted_lists[-1]))
+            sorted_lists.append([A[i]])
+        was_increasing = is_increasing
+
+    if not was_increasing:
+        sorted_lists[-1] = list(reversed(sorted_lists[-1]))
+
+    return list(heapq.merge(*sorted_lists))
+
+
+# time: O(n log n), but it's faster :')
+#       is n too small or k/n too large?
+# def sort_k_increasing_decreasing_array(A: List[int]) -> List[int]:
+#     return sorted(A)
 
 if __name__ == '__main__':
     exit(
