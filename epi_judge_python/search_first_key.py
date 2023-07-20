98fc3f1f2c4ddd18917ace8c73cd9cde5b410fd8
Andreas Willi <andiwilli8460@gmail.com>
2023-07-20 17:38:52 +0200

    Add 11.1 - first occurence of k in sorted array

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,11 +1,32 @@
 from typing import List
+import bisect
 
 from test_framework import generic_test
 
 
+# time: O(log n)
+# space: O(1)
+# def search_first_of_k(A: List[int], k: int) -> int:
+#     i = bisect.bisect_left(A, k)
+#     return i if (i < len(A) and A[i] == k) else -1
+
+
+# time: O(log n)
+# space: O(1)
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    lower = 0
+    upper = len(A) - 1
+
+    while lower < upper:
+        mid = lower + (upper - lower) // 2
+        if A[mid] < k:
+            lower = mid + 1
+        elif A[mid] > k:
+            upper = mid - 1
+        else:
+            upper = mid
+
+    return lower if (lower < len(A) and A[lower] == k) else -1
 
 
 if __name__ == '__main__':
