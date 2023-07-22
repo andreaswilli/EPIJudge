ac76eb457f78bae05ca8508525c73f2c42a24ac1
Andreas Willi <andiwilli8460@gmail.com>
2023-07-22 18:44:20 +0200

    Add 11.5 - compute real square root

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
+import math
 
 
+# time: O(log (x / s)), where s is the tolerance (precision)
+# space: O(1)
 def square_root(x: float) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    lower = 1 if x >= 1 else x
+    upper = x if x >= 1 else 1
+
+    while True:
+        mid = lower + (upper - lower) / 2
+        square = mid * mid
+
+        if math.isclose(square, x):
+            return mid
+        elif square < x:
+            lower = mid
+        else:
+            upper = mid
 
 
 if __name__ == '__main__':
