e4a10c19955c5132171e6fb0ded45e03c57d9528
Andreas Willi <andiwilli8460@gmail.com>
2023-07-23 21:09:05 +0200

    Add 11.6 - search in a 2d sorted array

--- epi_judge_python/search_row_col_sorted_matrix.py
+++ epi_judge_python/search_row_col_sorted_matrix.py
@@ -3,9 +3,21 @@ from typing import List
 from test_framework import generic_test
 
 
+# time: O(m + n), m = no. of rows, n = no. of cols
+# space: O(1)
 def matrix_search(A: List[List[int]], x: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    col = len(A[0]) - 1
+    row = 0
+
+    while col >= 0 and row < len(A):
+        if A[row][col] == x:
+            return True
+        elif A[row][col] < x:
+            row += 1
+        else:
+            col -= 1
+
+    return False
 
 
 if __name__ == '__main__':
