13354eb59217619b55ad3ad588a975a0c03b0f32
Andreas Willi <andiwilli8460@gmail.com>
2023-07-26 23:41:17 +0200

    Add 12.1 - test for palindromic permutations

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,9 +1,19 @@
+from typing import Counter
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(c), where c is the number of distinct characters in the string
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    counter = Counter(s)
+
+    num_odd = 0
+
+    for c in counter.values():
+        if c % 2:
+            num_odd += 1
+
+    return num_odd <= 1
 
 
 if __name__ == '__main__':
