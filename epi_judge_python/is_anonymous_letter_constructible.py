8facc2a3e5e0626c06a455a41b98c7d6b0c379b5
Andreas Willi <andiwilli8460@gmail.com>
2023-07-27 00:13:04 +0200

    Add 12.2 - is letter constructible

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,10 +1,31 @@
+from typing import Counter
 from test_framework import generic_test
 
 
+# pythonic solution
+# time: O(m + n)
+# space: O(L + M), where L and M are the numbers of distinct characters in the
+#                  letter and magazine, respectively
+# def is_letter_constructible_from_magazine(letter_text: str,
+#                                           magazine_text: str) -> bool:
+#     return len(Counter(letter_text) - Counter(magazine_text)) == 0
+
+
+# time: O(m + n)
+# space: O(L), where L is the number of distinct characters in the letter
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    letter_count = Counter(letter_text)
+
+    for c in magazine_text:
+        if c in letter_count:
+            letter_count[c] -= 1
+        if letter_count[c] == 0:
+            del letter_count[c]
+        if not letter_count:
+            break
+
+    return not letter_count
 
 
 if __name__ == '__main__':
