891488f88df6ef3114a11d5a5aaa87fd95039d7a
Andreas Willi <andiwilli8460@gmail.com>
2023-07-28 01:34:06 +0200

    Add 12.3 - isbn cache

--- epi_judge_python/lru_cache.py
+++ epi_judge_python/lru_cache.py
@@ -1,23 +1,31 @@
+from typing import OrderedDict
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
 class LruCache:
     def __init__(self, capacity: int) -> None:
-        # TODO - you fill in here.
-        return
+        self.capacity = capacity
+        self.ordered_dict = OrderedDict()
 
     def lookup(self, isbn: int) -> int:
-        # TODO - you fill in here.
-        return 0
+        if isbn not in self.ordered_dict:
+            return -1
+        price = self.ordered_dict.pop(isbn)
+        self.ordered_dict[isbn] = price
+        return price
 
     def insert(self, isbn: int, price: int) -> None:
-        # TODO - you fill in here.
-        return
+        if isbn in self.ordered_dict:
+            self.ordered_dict.move_to_end(isbn)
+        else:
+            self.ordered_dict[isbn] = price
+
+            if len(self.ordered_dict) > self.capacity:
+                self.ordered_dict.popitem(last=False)
 
     def erase(self, isbn: int) -> bool:
-        # TODO - you fill in here.
-        return True
+        return self.ordered_dict.pop(isbn, None) is not None
 
 
 def lru_cache_tester(commands):
