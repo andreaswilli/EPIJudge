a19352e1ae67ccd536f4a93809a38db691d82b96
Andreas Willi <andiwilli8460@gmail.com>
2023-07-30 23:39:21 +0200

    Add 12.4 - lowest common ancestor in tree

--- epi_judge_python/lowest_common_ancestor_close_ancestor.py
+++ epi_judge_python/lowest_common_ancestor_close_ancestor.py
@@ -8,9 +8,24 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+# time: O(h)
+# space: O(h)
 def lca(node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
+    visited = set()
+
+    while node0 or node1:
+        if node0 in visited:
+            return node0
+        if node0:
+            visited.add(node0)
+            node0 = node0.parent
+        if node1 in visited:
+            return node1
+        if node1:
+            visited.add(node1)
+            node1 = node1.parent
+
     return None
 
 
