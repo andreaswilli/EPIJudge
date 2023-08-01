7126f8d06f713fc200e42f28d119733d719314fa
Andreas Willi <andiwilli8460@gmail.com>
2023-08-01 20:48:20 +0200

    Add 12.5 - nearest repeated entries

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,11 +1,28 @@
-from typing import List
+from typing import Dict, List
+import typing
 
 from test_framework import generic_test
 
 
+# time: O(n)
+# space: O(n)
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    nearest_rep = float('inf')
+    word_occurance: Dict[str, int] = dict()
+
+    for i, word in enumerate(paragraph):
+        if word in word_occurance:
+            dist = i - word_occurance.get(word, 0)
+            if dist == 1:
+                return dist
+            if dist < nearest_rep:
+                nearest_rep = dist
+        word_occurance[word] = i
+
+    if nearest_rep == float('inf'):
+        nearest_rep = -1
+
+    return typing.cast(int, nearest_rep)
 
 
 if __name__ == '__main__':
