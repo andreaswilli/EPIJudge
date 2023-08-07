1d783eae4bfa2b22a7eeab80d3263e71a7857226
Andreas Willi <andiwilli8460@gmail.com>
2023-08-07 14:32:24 +0200

    Add 12.8 - longest subarray with distinct entries

--- epi_judge_python/longest_subarray_with_distinct_values.py
+++ epi_judge_python/longest_subarray_with_distinct_values.py
@@ -1,12 +1,54 @@
-from typing import List
+from typing import Counter, Dict, List
 
 from test_framework import generic_test
 
 
-def longest_subarray_with_distinct_entries(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+# time: O(n^2)
+# space: O(n)
+# def longest_subarray_with_distinct_entries(A: List[int]) -> int:
+#     def distinct(counter: Counter[int]) -> bool:
+#         for _, c in counter.items():
+#             if c > 1:
+#                 return False
+#         return True
+#
+#     max_length = 0
+#     left = 0
+#     right = 0
+#     counter: Counter[int] = Counter(A[0:1])
+#
+#     while left <= right < len(A):
+#         if distinct(counter):
+#             length = right - left + 1
+#             if length > max_length:
+#                 max_length = length
+#             right += 1
+#             counter.update(A[right:right+1])
+#         else:
+#             counter[A[left]] -= 1
+#             left += 1
+#
+#     return max_length
 
+
+# time: O(n)
+# space: O(n)
+def longest_subarray_with_distinct_entries(A: List[int]) -> int:
+    most_recent_occurence: Dict[int, int] = dict()
+    longest_subarray_start = 0
+    result = 0
+
+    for i, a in enumerate(A):
+        if a in most_recent_occurence:
+            prev_a_idx = most_recent_occurence[a]
+            if prev_a_idx >= longest_subarray_start:
+                length = i - longest_subarray_start
+                if length > result:
+                    result = length
+                longest_subarray_start = prev_a_idx + 1
+        most_recent_occurence[a] = i
+
+    return max(result, len(A) - longest_subarray_start)
 
 if __name__ == '__main__':
     exit(
