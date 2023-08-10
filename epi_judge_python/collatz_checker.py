8df162b996dcb0ae7190009066c435b5be5e4beb
Andreas Willi <andiwilli8460@gmail.com>
2023-08-10 22:29:14 +0200

    Add 12.11 - test the collatz conjecture

--- epi_judge_python/collatz_checker.py
+++ epi_judge_python/collatz_checker.py
@@ -1,9 +1,27 @@
+from typing import Set
 from test_framework import generic_test
 
 
 def test_collatz_conjecture(n: int) -> bool:
-    # TODO - you fill in here.
-    return False
+    confirmed_nums: Set[int] = set([1])
+
+    for x in range(2, n+1):
+        path: Set[int] = set()
+        cur = x
+        while cur < 2 ** 32:
+            if cur in path:
+                return False
+            path.add(cur)
+            if cur < x:
+                if cur in confirmed_nums:
+                    confirmed_nums.add(x)
+                break
+            if cur % 2:
+                cur *= 3
+                cur += 1
+            else:
+                cur //= 2
+    return True
 
 
 if __name__ == '__main__':
